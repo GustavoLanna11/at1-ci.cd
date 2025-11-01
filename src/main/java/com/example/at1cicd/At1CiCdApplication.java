f3fe6aa83c1d467d35bf512273b36d9a4277a180
Gustavo Lanna <gustavomlanna@gmail.com>
2025-11-01 15:29:23 -0300

    :sparkles: feat: estruturação inicial

---
+++ src/main/java/com/example/at1cicd/At1CiCdApplication.java
@@ -0,0 +1,14 @@
+package com.example.at1cicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class At1CiCdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(At1CiCdApplication.class, args);
+    }
+
+}
+
